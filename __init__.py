dc3b06ede7eead739d515b88e62b0a3eab5da69f
remrama <mallett.remy@gmail.com>
2023-03-01 12:25:28 -0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from .datasets import *
-
-# Define froid logging style.
-logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", datefmt="%Y-%b-%d %H:%M:%S")
-
-__author__  = "Remington Mallett <mallett.remy@gmail.com>"
-__version__ = "0.0.0"
